fcd4daf5116d24bd66f87948e84d9d0d819556a8
stabyte <71367205+Sta-byte@users.noreply.github.com>
2021-02-07 16:45:11 +0300

    Add files via upload

--- simple.py
+++ simple.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import streamlit as st
-st.title('SIMPLE STREAMLIT APP')
+st.title('  Welcome to My App')
 st.text ('type a number in the box below')
 n=st.number_input('Number=int')
 st.write(f'{n} +1={n+1}')
 s=st.text_input('Tpye a namein the box below')
 st.write(f'Hello {s}')
+a=st.text_input('Tpye your age the box below')
+st.write(f'You are {a} years old')
