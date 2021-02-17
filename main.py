408f7be52a5a78425e5e53adf7bd1db594d14863
stabyte <71367205+Sta-byte@users.noreply.github.com>
2021-02-17 13:27:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #import bs4
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
